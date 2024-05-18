3fe8fdcbde4616777dd47861446f119b5bed87fb
alam7865 <sabbajalam5@gmail.com>
2024-05-18 20:18:01 +0530

    change

--- practise/hello.java
+++ practise/hello.java
@@ -2,6 +2,34 @@ import java.util.*;
 
 public class hello {
     public static void main(String[] args) {
-        System.out.println("Hello Sabaz Alam");
+        int n = 4;
+        int k = 2;
+
+        int arr[] = { 100, 200, 300, 400 };
+
+        int sum = 0;
+        int i = 0;
+        int max = 0;
+
+        if (arr.length < k) {
+            // break;
+        }
+
+        while (i < k) {
+            sum += arr[i];
+            i++;
+        }
+
+        int x1 = 0;
+
+        max = Math.max(max, sum);
+        for (int x = k; x < arr.length; x++) {
+            sum -= arr[x1];
+            x1++;
+            sum += arr[x];
+            max = Math.max(max, sum);
+        }
+
+        System.out.println("Max: " + max);
     }
 }
